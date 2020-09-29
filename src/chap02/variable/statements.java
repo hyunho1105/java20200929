70cff6833d26eafe95de7e15750f97ea5f8ad875
hyunho1105 <hyunho1105@gmail.com>
2020-09-29 11:40:22 +0900

    frist

---
+++ src/chap02/variable/statements.java
@@ -0,0 +1,18 @@
+package chap02.variable;
+
+public class statements {
+	public static void main(String[] atgs) {
+		System.out.println("첫번째 명령문");
+		System.out.println("두번째 명령문");
+		System.out.println("세번째 명령문");
+		
+		// 한 줄 수석
+		/*
+		 * 
+		 *  여러 줄 주석
+		 *  여러 줄 주석
+		 * 
+		 * 
+		 */
+	}
+}
