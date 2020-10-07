270abd0d0097348adc57011bfd0ffc5634a49908
hyunho1105 <hyunho1105@gmail.com>
2020-10-07 17:50:21 +0900

    Oct 7

--- src/chap04/exercises/Exercise04.java
+++ src/chap04/exercises/Exercise04.java
@@ -1,5 +1,16 @@
 package chap04.exercises;
 
 public class Exercise04 {
-
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		while (sum != 5) {
+			int num = (int) (Math.random() * 6) + 1;
+			int num2 = (int) (Math.random() * 6) + 1;
+			sum = num+num2;
+			System.out.println("("+ num + "," + num2 + ")");
+			
+		}
+		
+	}
 }
---
+++ src/chap04/exercises/Exercise06.java
@@ -0,0 +1,12 @@
+package chap04.exercises;
+
+public class Exercise06 {
+	public static void main(String[] args) {
+		for (int s=1; s<=5;s++) {
+			for (int n=1;n<=s;n++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
